ec4231e11b887d62e11482a15bcfde93eac1fa76
nkysingh <nkysingh01@gmail.com>
2020-07-05 15:15:35 +0530

    setting file changes

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r+@p(st&*109s15drs#)n1uwltvqh*7auln^xc7jkszyxp%*wk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- webapp/urls.py
+++ webapp/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
  path('', views.index, name='index'),
  path('patient-registrations-landing/', views.patient_registrations_landing, name='patient_registrations_landing'),
  path('patient-registrations/', views.patient_registrations, name='patient_registrations'),
- path('patient-deatils/', views.patient_details, name='patient_details')
+ path('patient-details/', views.patient_details, name='patient_details')
 
 ]
